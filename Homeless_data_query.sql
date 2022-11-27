d05d721bcde0f0024609403fc0c9f5e15754cd30
Patrick Gatal <62809624+PatrickIV@users.noreply.github.com>
2022-11-27 01:17:21 -0500

    Update Homeless_data_query.sql

--- Homeless_data_query.sql
+++ Homeless_data_query.sql
@@ -1,9 +1,43 @@
--- Looking at Totals vs Homeless in emergency shelters
+/*
+Evaluating Areas on their homelessness support based off multiple factors:
+	- unsheltered vs sheltered homeless people
+    - How many people in Emergency Shelters
+    - How many people in Transitional Housing
+    - How many people in Safe Haven Housing
+    - How effective is the Transitional Housing
+*/
+-- Looking at unsheltered vs sheltered homeless people
 Select 
 	CoC_Name as Location,
     Count_Year as Year,
     Overall_Homeless as Total,
-    Sheltered_ES_Homeless as Emergency_Shelters
+    Unsheltered_Homeless as Unsheltered,
+    (Unsheltered_Homeless/Overall_Homeless)*100 as Percentage
 From 
 	homelessness.homeless_data
-order by 1,2
+Where
+	Count_Year = 2014
+order by 5 DESC
+-- Looking at Total Sheltered Homeless vs Homeless in emergency shelters
+Select 
+	CoC_Name as Location,
+    Count_Year as Year,
+    Homeless_Individuals as Total,
+    Sheltered_ES_Homeless as Emergency_Shelters,
+    (Sheltered_ES_Homeless/Overall_Homeless)*100 as Percentage
+From 
+	homelessness.homeless_data
+Where
+	Count_Year = 2014
+order by 5 DESC
+-- Looking at Total Sheltered Homeless vs Homeless in Transitional Housing
+Select 
+	CoC_Name as Location,
+    Count_Year as Year,
+    Homeless_Individuals as Total,
+    Sheltered_ES_Homeless as Emergency_Shelters,
+    (Sheltered_ES_Homeless/Overall_Homeless)*100 as Percentage
+From 
+	homelessness.homeless_data
+Where
+	Count_Year = 2014
