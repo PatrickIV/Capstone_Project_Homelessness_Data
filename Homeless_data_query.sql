d5220337480b2bf932d058e51e231cc7cb714b58
Patrick Gatal <62809624+PatrickIV@users.noreply.github.com>
2022-11-27 17:48:06 -0500

    Update Homeless_data_query.sql

--- Homeless_data_query.sql
+++ Homeless_data_query.sql
@@ -22,9 +22,9 @@ order by 5 DESC
 Select 
 	CoC_Name as Location,
     Count_Year as Year,
-    Homeless_Individuals as Total,
+    Overall_Homeless as Total,
     Sheltered_ES_Homeless as Emergency_Shelters,
-    (Sheltered_ES_Homeless/Overall_Homeless)*100 as Percentage
+    ROUND((Sheltered_ES_Homeless/Overall_Homeless)*100, 2) as Percentage
 From 
 	homelessness.homeless_data
 Where
@@ -34,10 +34,50 @@ order by 5 DESC
 Select 
 	CoC_Name as Location,
     Count_Year as Year,
-    Homeless_Individuals as Total,
-    Sheltered_ES_Homeless as Emergency_Shelters,
-    (Sheltered_ES_Homeless/Overall_Homeless)*100 as Percentage
+    Overall_Homeless as Total,
+    Sheltered_TH_Homeless as Transitional_Housing,
+    ROUND((Sheltered_TH_Homeless/Overall_Homeless)*100, 2) as Percentage
 From 
 	homelessness.homeless_data
 Where
 	Count_Year = 2014
+order by 5 DESC
+-- Looking at Total Sheltered Homeless vs Homeless in Safe Haven Housing
+Select 
+	CoC_Name as Location,
+    Count_Year as Year,
+    Overall_Homeless as Total,
+    Sheltered_SH_Homeless as SafeHaven_Housing,
+    ROUND((Sheltered_SH_Homeless/Overall_Homeless)*100, 2) as Percentage
+From 
+	homelessness.homeless_data
+Where
+	Count_Year = 2014
+order by 5 DESC
+-- Looking at effectiveness of the Transitional Housing
+with Overtime (Location, Year, Total, Homelessness_Overtime)
+as
+(
+Select 
+	CoC_Name as Location,
+    Count_Year as Year,
+    Overall_Homeless as Total,
+    Overall_Homeless - LAG(Overall_Homeless) OVER (Partition by CoC_Name order by Coc_Name, Count_Year) as Homelessness_Overtime
+From 
+	homelessness.homeless_data
+order by 1
+)
+-- Calculating effectiveness of Transitional Housing
+loss_or_Gain_of_Homeless 
+as
+(
+Select
+	Location,
+    Homelessness_Overtime
+From 
+	Overtime
+Where
+	Year = 2014
+)
+Select *
+From loss_or_Gain_of_Homeless
